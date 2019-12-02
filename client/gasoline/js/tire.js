95d6fb15ecf4123be9cb56d17c73ceb30015e70b
Carin Hansen <carin@burst-digital.com>
2019-12-02 12:43:47 +0100

    feat(Gasoline) add tires and remove energy drink

---
+++ client/gasoline/js/tire.js
@@ -0,0 +1,11 @@
+import { Food } from './food.js';
+export class Tire extends Food {
+    constructor() {
+        super();
+        this._element = document.createElement("tire");
+        let foreground = document.getElementsByTagName("foreground")[0];
+        foreground.appendChild(this._element);
+    }
+    action() {
+    }
+}
