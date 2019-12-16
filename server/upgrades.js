d87cc79af4d656c08ab3cc98094fa8d343c704dd
Perry Janssen <perry@burst-digital.com>
2019-12-16 17:43:32 +0100

    reformat

--- server/upgrades.js
+++ server/upgrades.js
@@ -1,33 +1,33 @@
-const { setStat, getStat } = require('./db');
+const {setStat, getStat} = require('./db');
 
 const upgradeList = [
-  { upgrade: 'rain-tires' },
-  { upgrade: 'engine-upgrade' },
-  { upgrade: 'shotgun' },
+    {upgrade: 'rain-tires'},
+    {upgrade: 'engine-upgrade'},
+    {upgrade: 'shotgun'},
 ];
 
 /**
  * Set all upgrades to FALSE in the database.
  */
 const resetUpgrades = async () => {
-  let emptyUpgrades = {};
+    let emptyUpgrades = {};
 
-  upgradeList.forEach(upgrade => {
-    emptyUpgrades[upgrade.upgrade] = false;
-  });
+    upgradeList.forEach(upgrade => {
+        emptyUpgrades[upgrade.upgrade] = false;
+    });
 
-  setStat('upgrades', emptyUpgrades);
+    setStat('upgrades', emptyUpgrades);
 };
 
 /**
  * Add upgrade to the database.
- * 
- * @param {string} upgrade 
+ *
+ * @param {string} upgrade
  */
 const addUpgrade = async (upgrade) => {
-  let upgrades = await getStat('upgrades');
-  upgrades[upgrade] = true;
-  setStat('upgrades', upgrades);
+    let upgrades = await getStat('upgrades');
+    upgrades[upgrade] = true;
+    setStat('upgrades', upgrades);
 };
 
-module.exports = { addUpgrade, resetUpgrades };
+module.exports = {addUpgrade, resetUpgrades};
