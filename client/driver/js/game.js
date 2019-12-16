841be401fa8bedca21848ea3c7745d8a83d85bfe
Perry Janssen <perry@burst-digital.com>
2019-12-16 13:49:24 +0100

    update js files

--- client/driver/js/game.js
+++ client/driver/js/game.js
@@ -2,6 +2,7 @@ import { Car } from "./car.js";
 import { Opponent } from "./opponent.js";
 import { Dialog } from "./dialog.js";
 import { Pitstop } from "./pitstop.js";
+import { Message } from "./message.js";
 export class Game {
     constructor() {
         this._fps = 30;
@@ -11,23 +12,32 @@ export class Game {
         this.opponent = [];
         this.inPitstop = false;
         this.distance = 0;
+        this.speed = 1;
         this.lap = 1;
+        this.lapTime = {};
         this.lapText = 'Ronde 1 van 4';
         this._fpsInterval = 1000 / this._fps;
         this._then = Date.now();
         this.createCar();
-        this.startTime = Date.now();
         this.socket = io({ timeout: 60000 });
         this.socket.emit('driver:start', {
             uuid: this.getCookie('uuid'),
         });
-        this.socket.on('server:aero:boost', data => {
-            console.log('BOOST!');
+        this.socket.on('server:aero:boost', (data) => {
+            this.currentMessage = new Message('Aerodynamische boost!', '+ 10% snelheid', 'good');
+            this.speed += .1;
         });
-        this.socket.on('server:pitstop:done', data => {
+        this.socket.on('server:aero:slow', (data) => {
+            this.currentMessage = new Message('Aerodynamische slowdown!', '- 10% snelheid', 'bad');
+            if (this.speed >= .3) {
+                this.speed -= .1;
+            }
+        });
+        this.socket.on('server:pitstop:done', (data) => {
             this.lap++;
             this.lapText = 'Ronde ' + this.lap + ' van 4';
             this.scoreElement.innerText = this.lapText;
+            this.startTime = Date.now();
             this.inPitstop = false;
             this.pitstopObject.hide();
             this.pitstopObject = null;
@@ -36,6 +46,9 @@ export class Game {
         this.scoreElement.classList.add('lap');
         this.scoreElement.innerText = this.lapText;
         document.body.appendChild(this.scoreElement);
+        this.distanceElement = document.createElement('div');
+        this.distanceElement.classList.add('distance');
+        document.body.appendChild(this.distanceElement);
         this.gameLoop();
     }
     static getInstance() {
@@ -46,6 +59,10 @@ export class Game {
     }
     startGame() {
         this.running = true;
+        this.startTime = Date.now();
+    }
+    finish() {
+        console.log('RACE FINISHED!!!');
     }
     gameLoop() {
         requestAnimationFrame(() => this.gameLoop());
@@ -68,10 +85,16 @@ export class Game {
                         this.opponent.push(opponent);
                     }
                 }
-                this.distance++;
-                if (this.distance > 100) {
-                    this.pitstop();
-                    this.distance = 0;
+                this.distance += this.speed;
+                this.distanceElement.innerHTML = this.distance < 1000 ? (1000 - this.distance).toFixed(0).toString() + '<br>' + (this.speed * 200).toFixed(0).toString() + ' km/h' : '0';
+                if (this.distance > 1000) {
+                    if (this.lap >= 4) {
+                        this.finish();
+                    }
+                    else {
+                        this.pitstop();
+                        this.distance = 0;
+                    }
                 }
                 this._then = now - (elapsed % this._fpsInterval);
             }
@@ -98,43 +121,38 @@ export class Game {
         this._car = new Car();
     }
     pitstop() {
+        this.lapTime[this.lap] = Date.now() - this.startTime;
+        console.log(this.lapTime);
         this.socket.emit('driver:pitstop');
         this.inPitstop = true;
+        this.socket.emit('driver:lap', {
+            lap: this.lap,
+            time: this.lapTime,
+        });
     }
     checkCollision() {
         for (let i = 0; i < this.opponent.length; i++) {
-            if (this._car._element.getBoundingClientRect().left < this.opponent[i].element.getBoundingClientRect().right &&
+            if (!this._car.hit &&
+                this._car._element.getBoundingClientRect().left < this.opponent[i].element.getBoundingClientRect().right &&
                 this._car._element.getBoundingClientRect().right > this.opponent[i].element.getBoundingClientRect().left &&
                 this._car._element.getBoundingClientRect().bottom > this.opponent[i].element.getBoundingClientRect().top &&
                 this._car._element.getBoundingClientRect().top < this.opponent[i].element.getBoundingClientRect().bottom) {
                 if (!document.querySelector('.opponentHit')) {
+                    this._car.hit = true;
+                    this.speed -= .1;
                     this.opponentHit = document.createElement('img');
                     this.opponentHit.classList.add('opponentHit');
                     this.opponentHit.src = "";
                     this.opponentHit.src = "./img/explosion.gif";
                     document.body.appendChild(this.opponentHit);
                     this.opponentHit.style.transform = `translate(${this._car.posX - 80}px, ${this._car.posY}px)`;
+                    this._car._element.classList.add('blinking');
                     setTimeout(() => {
                         this.opponentHit.remove();
                         this._car.hit = false;
-                    }, 5000);
+                        this._car._element.classList.remove('blinking');
+                    }, 3000);
                 }
-            }
-        }
-    }
-    spawnOpponent(amount) {
-        let opponent = [];
-        for (let i = 0; i < amount; i++) {
-            opponent.push(new Opponent());
-        }
-        return opponent;
-    }
-    checkCar() {
-        if (this._carTime > this._fps * 0) {
-            if (!this._car) {
-                this._car = new Car();
-                this.opponentHit.style.transform = `translate(${this._car.posX - 80}px, ${this._car.posY}px)`;
-                setTimeout(() => { this.opponentHit.classList.remove('opponentHit'); this.opponentHit.remove(); console.log('timeout klaar'); }, 4000);
             }
         }
     }
@@ -143,6 +161,7 @@ export class Game {
         const parts = value.split("; " + name + "=");
         if (parts.length == 2)
             return parts.pop().split(";").shift();
+        return null;
     }
 }
 window.addEventListener("load", () => {
